ba43e34313d3e9a3e14af143190522946f3e0708
wzg <wzg0312@163.com>
2018-09-12 16:19:02 +0800

    add p2p singleton code

--- node/node.go
+++ node/node.go
@@ -1,22 +1,19 @@
 package node
 
 import (
-
 	"github.com/linkchain/common"
-	"github.com/linkchain/p2p"
-	"github.com/linkchain/consensus"
 	"github.com/linkchain/common/util/log"
+	"github.com/linkchain/consensus"
+	"github.com/linkchain/p2p"
 )
-
 
 var (
 	//service collection
-	 svcList = []common.IService{
-		 &p2p.Service{},
-		 &consensus.Service{},
-	 }
+	svcList = []common.IService{
+		p2p.GetP2PServer(),
+		&consensus.Service{},
+	}
 )
-
 
 func Init() {
 	log.Info("Node init...")
@@ -27,11 +24,11 @@ func Init() {
 	}
 }
 
-func Run(){
+func Run() {
 	log.Info("Node is running...")
 
 	//start all service
-	for _,v := range svcList{
+	for _, v := range svcList {
 		v.Start()
 	}
 
