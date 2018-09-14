ab9d3096825b52de79d681150cbfaa3d6323b45b
wzg <wzg0312@163.com>
2018-09-14 11:04:37 +0800

    add full mode code

--- sync/sync.go
+++ sync/sync.go
@@ -1,4 +1,31 @@
 package sync
 
 import (
+	_ "github.com/linkchain/consensus/manager"
+	"github.com/linkchain/sync/full"
 )
+
+var (
+	engine full.ProtocolManager
+)
+
+type Service struct {
+}
+
+func (s *Service) Init(i interface{}) bool {
+	//log.Info("sync service init...");
+	engine = full.ProtocolManager{}
+	engine.Init(i)
+	return true
+}
+
+func (s *Service) Start() bool {
+	//log.Info("sync service start...");
+	engine.Start()
+	return true
+}
+
+func (s *Service) Stop() {
+	//log.Info("sync service stop...");
+	engine.Stop()
+}
